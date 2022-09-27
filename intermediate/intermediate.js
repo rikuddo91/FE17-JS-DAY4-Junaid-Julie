45a013edda9e9af8e3625ff462aa346ea2df34b5
rikuddo91 <gullkhan0@live.com>
2022-09-27 13:29:25 +0200

    Intermediate_EX-Finished

---
+++ intermediate/intermediate.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.santa').forEach(item => {
+    item.addEventListener('click', removeAll);
+})
+
+function removeAll() {
+    if (this.style.opacity == "0") {
+        this.style.opacity = "1";
+        
+    } else {
+        this.style.opacity = "0";
+        this.style.transition = '1s'
+    }
+}
